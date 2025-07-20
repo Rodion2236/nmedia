43b1a8d92678066debbafd8ded5196d83586d956
Rodion <kosolapkinr@gmail.com>
2025-07-20 10:57:13 +0400

    fix countFormatter

--- app/src/main/java/ru/netology/nmedia/util/CountFormatter.kt
+++ app/src/main/java/ru/netology/nmedia/util/CountFormatter.kt
@@ -4,20 +4,20 @@ fun formatCount(count: Int): String {
     return when {
         count < 1_000 -> "$count"
         count < 10_000 -> {
-            val value = count / 1_000f
-            if (value % 1 == 0f) {
-                "${value.toInt()}K"
+            val value = count / 100
+            if (value % 10 == 0) {
+                "${value / 10}K"
             } else {
-                "${String.format("%.1f", value)}K"
+                "${value / 10}.${value % 10}K"
             }
         }
         count < 1_000_000 -> "${count / 1_000}K"
         count < 10_000_000 -> {
-            val value = count / 1_000_000f
-            if (value % 1 == 0f) {
-                "${value.toInt()}M"
+            val value = count / 100_000
+            if (value % 10 == 0) {
+                "${value / 10}M"
             } else {
-                "${String.format("%.1f", value)}M"
+                "${value / 10}.${value % 10}M"
             }
         }
         else -> "${count / 1_000_000}M"

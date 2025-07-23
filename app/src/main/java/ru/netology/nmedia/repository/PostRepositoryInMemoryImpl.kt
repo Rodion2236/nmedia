8e1863aa3f52c152d7ae2e10c0fbfb382d1e5230
Rodion <kosolapkinr@gmail.com>
2025-07-23 22:35:40 +0400

    implement mvvm

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -0,0 +1,54 @@
+package ru.netology.nmedia.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.dto.Post
+
+class PostRepositoryInMemoryImpl: PostRepository {
+
+    private var post = Post(
+        id = 1,
+        author = "Нетология. Университет интернет-профессий будущего",
+        content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+        published = "21 мая в 18:36",
+        likes = 0,
+        likedByMe = false
+    )
+
+    private val _data = MutableLiveData(post)
+    override fun get(): LiveData<Post> = _data
+
+    override fun like() {
+        post = post.copy(
+            likedByMe = !post.likedByMe,
+            likes = if (post.likedByMe) {
+                post.likes -1
+            } else {
+                post.likes +1
+            }
+        )
+
+        _data.value = post
+    }
+
+    override fun share() {
+        post = post.copy(
+            shares = post.shares +1
+        )
+
+        _data.value = post
+    }
+
+    private var isViewed = false
+
+    override fun views() {
+        if (isViewed) return
+        isViewed = true
+
+        post = post.copy(
+            views = post.views +1
+        )
+
+        _data.value = post
+    }
+}
---
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
@@ -0,0 +1,16 @@
+package ru.netology.nmedia.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+
+class PostViewModel: ViewModel() {
+
+    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    val data: LiveData<Post> = repository.get()
+    fun like() = repository.like()
+    fun share() = repository.share()
+    fun views() = repository.views()
+}
